e37f7c6a45b1a15fd3558a8ba2404f91806bf020
lumanyano654 <gomomolumanyano@gmail.com>
2020-09-29 17:35:37 +0200

    added an factory function

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var exphbs = require('express-handlebars')
 var bodyparser = require('body-parser')
-
+var medsApp = require('./meds-app')
 
 var app = express()
+
+const medApp = medsApp()
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -12,16 +14,19 @@ app.use(bodyparser.urlencoded({extended: false}))
 app.set(bodyparser.json())
 
 app.get('/', function (req,res){
-    
-    res.render("index")
+    res.render('index')
 })
 
 app.post("/summary", function(req,res){
+    const med =  medApp.diffCode(req.body.code)
+    console.log(med);
     
-    var actualCode = req.body.code
-
-
-    res.render('summary', {enterCode: actualCode})
+    if (med) {
+        res.render('summary', {})
+    } else {
+        res.render("index")
+    }
+    // res.render('summary', {enterCode: actualCode})
 })
 
 var PORT = process.env.PORT || 3008;
---
+++ meds-app.js
@@ -0,0 +1,25 @@
+module.exports = function medsApp(){
+
+    var codeList = {}
+
+
+
+    function diffCode(code){
+        var list = ["3656","4546","3233","4464"]
+
+
+        if (list.includes(code) ){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+
+    return{
+        diffCode
+
+
+        }
+}
